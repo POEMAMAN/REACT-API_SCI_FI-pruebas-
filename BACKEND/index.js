687274ff5f0773628eb6caf7e6262469851c3a82
poemaman <luis.campos.pousa@gmail.com>
2024-01-14 14:20:47 +0100

    integration

---
+++ BACKEND/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require("cors")
+const booksRouter = require("./src/api/routes/book.routes");
+const collectionsRouter = require("./src/api/routes/collection.routes");
+const universeRouter = require("./src/api/routes/universe.routes");
+const authorsRouter = require("./src/api/routes/author.routes");
+const userRouter = require('./src/api/routes/user.routes');
+dotenv.config();
+
+const {connect} = require('./src/utils/db');
+const { isAuth } = require('./src/middlewares/auth');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+connect();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/books", booksRouter);
+app.use("/collections", collectionsRouter)
+app.use("/author", authorsRouter)
+app.use("/universe", universeRouter)
+app.use('/user', userRouter);
+
+app.listen(PORT, () => console.log('listening on port ' + PORT));
--- REACT-BACK-NEW/src/api/routes/ciudad.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const {getCiudades, getOneCiudad, postCiudad} = require('../controllers/ciudad.controller');
-const ciudadRoutes = express.Router();
-
-ciudadRoutes.get('/', getCiudades);
-ciudadRoutes.get('/:id', getOneCiudad);
-ciudadRoutes.post('/', postCiudad);
-
-module.exports  = ciudadRoutes;
